c13ff9a17dad32ff2a23f95017febec48b6ceca3
EmaCartoon <emacartoon@gmail.com>
2021-03-23 22:46:57 -0700

    Almost have it working, need to get the forLoop for the Cards to work.

---
+++ dist/generateHTML.js
@@ -0,0 +1,63 @@
+const Employee = require("../lib/Employee.js");
+const Engineer = require("../lib/Engineer.js");
+const Intern = require("../lib/Intern.js");
+const Manager = require("../lib/Manager.js");
+const members = require("/index.js");
+
+const cards = [];
+
+const header =
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../dist/style.css" />
+    <title>Team Profile</title>
+</head>
+<body>
+    <header>
+        <h1>Team Profile</h1>
+    </header>
+    <nav>
+        <p>&nbsp</p>
+    </nav>
+    <main class="team">
+        <div class="row">
+`;
+
+Array.prototype.getRandomItem = function(){
+
+function Manager(name, ) {
+    cards.forEach(members) => {
+        return `
+        <div class="card">
+        <div class="cardhead">
+        <p id="name">${data.name}</p>
+        <p id="role">${data.role}</p>
+        </div>
+        <div class="cardbody">
+        <div id="id" class="revver"><p>${data.id}</p></div>
+        <div id="email" class="revver"><p>E-mail: <a href="mailto:${data.email}">${data.email}</a></p></div>
+        <div id="dependent" class="revver"><p>${data.dep}</p></div>
+        </div>
+        </div>`
+    };
+    
+};
+
+const footer =`
+            </div>
+</main>
+<footer>
+<p>&copy; 2021 Emily "Emma" Lysyk</p>
+</footer>
+</body>
+</html>`;
+    
+function generateHTML() {
+    return header + cards + footer;
+}
+
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,10 +1,171 @@
 const { TestScheduler } = require("@jest/core");
-const Employee = require("../lib/Employee");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./dist/generateHTML");
+const members = [];
 
-describe("Employee", ( => {
-    describe("Initialization", () => {
-        test("Can initialize an employee object", () => {
-            const e = new Employee();
-        })
-    })
-}))
+// describe("Employee", ( => {
+//     describe("Initialization", () => {
+//         test("Can initialize an employee object", () => {
+//             const e = new Employee();
+//         })
+//     })
+// }));
+
+determineEmployee()
+function determineEmployee(){
+    inquirer
+      .prompt([
+    {
+      type: "list",
+      name: "role",
+      message: "What's the position?",
+      choices: ["Manager", "Engineer", "Intern", "That's all of them"],
+    },
+])
+.then((answer) => {
+    switch (answer.role) {
+        case "Manager":
+            managerData();
+            break;
+        
+        case "Engineer":
+            engineerData();
+            break;
+        
+        case "Intern":
+            internData();
+            break;
+        
+        case "That's all of them":
+            generateHTML();
+            break;
+        
+    }
+});
+}
+
+function managerData(){
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "number",
+      name: "id",
+      message: "What is your Employee's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your e-mail address?",
+    },
+    {
+      type: "number",
+      name: "roomNumber",
+      message: "What is your room number?",
+    //   when: (answers) => answers.role === "Manager",
+    },
+])
+.then((answer) => {
+    const manager = new Manager(
+        answer.name,
+        answer.id,
+        answer.email,
+        dep = (`Room Number: ` + answer.roomNumber),
+    );
+    members.push(manager);
+    determineEmployee();
+});
+}
+
+function engineerData(){
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the Employee's name?",
+    },
+    {
+      type: "number",
+      name: "id",
+      message: "What is the Employee's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the Employee's e-mail address?",
+    },
+    {
+       type: "input",
+       name: "github",
+       message: "What is their Github username?",
+    //    when: (answers) => answers.role === "Engineer",
+    },
+])
+.then((answer) => {
+    const engineer = new Engineer (
+        answer.name,
+        answer.id,
+        answer.email,
+        dep = (`Github: ` + `<a href="http://www.github.com/` + answer.github +`">` + answer.github + `</a>`)
+    );
+    members.push(engineer);
+    determineEmployee();
+});
+}
+
+
+function internData(){
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the Employee's name?",
+    },
+    {
+      type: "number",
+      name: "id",
+      message: "What is the Employee's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the Employee's e-mail address?",
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school do they go to?",
+     //    when: (answers) => answers.role === "Intern",
+     },
+ 
+])
+.then((answer) => {
+    const intern = new Intern (
+        answer.name,
+        answer.id,
+        answer.email,
+        dep = (`School: `+ answer.school)
+    );
+    members.push(intern);
+    determineEmployee();
+});
+}
+
+
+function saveTheFile(data) {
+     fs.writeFile("./dist/index.html", generateHTML(data), function (err) {
+      if (err) {
+          return console.log(err);
+     }
+      console.log("File created");
